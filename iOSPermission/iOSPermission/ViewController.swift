514d62ba9c5ccbaa84ac922f526fcb873b40eb9d
Senthil kumar K <nightrider.cbe@gmail.com>
2020-05-22 00:21:01 +0530

    Commit

--- iOSPermission/iOSPermission/ViewController.swift
+++ iOSPermission/iOSPermission/ViewController.swift
@@ -9,15 +9,26 @@
 import UIKit
 import SPPermission
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,SPPermissionDialogDelegate,SPPermissionDialogDataSource {
 
     @IBAction func Click_TouchUpInside(_ sender: Any) {
-        SPPermission.Dialog.request(with: [.camera,.locationWhenInUse,.microphone], on: self)
         
+        // Set permission
+     SPPermission.Dialog.request(with: [.camera,.locationWhenInUse,.microphone], on: self)
+       
+      
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Check Conditopn
+        let Location : Bool = SPPermission.isAllowed(.locationWhenInUse)
+              if Location == true {
+                  print("Location is enabled")
+              }else {
+                  print("Location is Disabled")
+              }
     }
 
 
